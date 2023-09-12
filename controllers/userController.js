f04f53f2b0813f13f4d7c6b7ce54cb9924f6ba33
cmdnguyen <cmdnguyen94@gmail.com>
2023-09-11 22:04:31 -0500

    addFriend and removeFriend now works, README started

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,22 +74,48 @@ module.exports = {
   async addFriend (req, res) {
     try {
       console.log('Adding a friend')
-      console.log(req.body)
+
+    // Verify that the user ID in req.params.userId is valid
+    console.log("User ID to add a friend for:", req.params.userId);
       const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
-        { $addToSet: {friends: req.body} },
+        { $addToSet: {friends: req.body.userId} },
         { runValidators: true, new: true })
       
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
       }
-      console.log(user)
+      console.log("Updated User:", user);
       res.json(user)
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
-  }
+  },
+
+  async removeFriend(req, res) {
+    try {
+      console.log('Deleting a friend')
+      console.log('User:', req.params.userId)
+      console.log('Friend:', req.params.friendId)
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId  } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
 
 
